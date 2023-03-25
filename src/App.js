841170e665d348f5847eb0234865145a55018ed4
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-03-25 13:09:32 -0700

    week 2 workshop

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='contact' element={<ContactPage />}  />
-        <Route path='directory' element={<CampsitesDirectoryPage />}  />
-        <Route 
-            path='directory/:campsiteId'
-            element={<CampsiteDetailPage />}
-        />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route path='about' element={<AboutPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
