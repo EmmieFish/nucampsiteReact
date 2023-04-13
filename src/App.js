f3f56589fd8e10f476aa26c5de40cb59a4df2307
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-04-12 22:42:59 -0700

    fetch async updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-import React from 'react';
-import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -8,8 +8,17 @@ import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
+import "./App.css";
 
 function App() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+    }, [dispatch]);
+
     return (
         <div className="App">
             <Header />
